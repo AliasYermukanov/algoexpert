fba1b1decf43c2a4d44ab7ef8783dee393e7faa0
Alias Yermukanov <ermukanovalias@mail.ru>
2023-09-28 10:47:47 +0600

    added new tasks

--- depth-first-search/main.go
+++ depth-first-search/main.go
@@ -1,1 +1,48 @@
-package depth_first_search
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+func main() {
+	var graph = NewNode("A").AddChildren("B", "C", "D")
+	graph.Children[0].AddChildren("E").AddChildren("F")
+	graph.Children[2].AddChildren("G").AddChildren("H")
+	graph.Children[0].Children[1].AddChildren("I").AddChildren("J")
+	graph.Children[2].Children[0].AddChildren("K")
+
+	start := time.Now()
+	fmt.Println(graph.DepthFirstSearch([]string{}))
+	elapsed := time.Since(start)
+	log.Printf("Binomial took %s", elapsed)
+}
+
+func (n *Node) DepthFirstSearch(array []string) []string {
+	array = append(array, n.Name)
+	for _, child := range n.Children {
+		array = child.DepthFirstSearch(array)
+	}
+	return array
+}
+
+type Node struct {
+	Name     string
+	Children []*Node
+}
+
+func NewNode(name string) *Node {
+	return &Node{
+		Name:     name,
+		Children: []*Node{},
+	}
+}
+
+func (n *Node) AddChildren(names ...string) *Node {
+	for _, name := range names {
+		child := Node{Name: name}
+		n.Children = append(n.Children, &child)
+	}
+	return n
+}
